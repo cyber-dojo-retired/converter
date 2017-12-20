bdb2f58bdaab332ebe4454291a096fc2845125e9
Jon Jagger <jon@jaggersoft.com>
2017-12-20 07:52:35 +0000

    added script to extract a single katas tgz file

---
+++ tar_1_kata.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# script to extract a single kata from a 256th tgz file
+# created using get256th.sh
+
+readonly outer=$1  # eg 42
+readonly inner=$2  # eg 1AFD7EC5
+
+readonly my_dir="$( cd "$( dirname "${0}" )" && pwd )"
+
+tar -zcf ${1}${2}.tgz -C . ${1}/${2}/
